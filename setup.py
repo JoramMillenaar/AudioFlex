7dc4a3fd35aaab230ba73e868632c25959107172
jofoks <joormillenaar@live.nl>
2024-01-20 15:49:10 +0100

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(
+    name='AudioFlex',
+    description='A Pure Python Suite of Algorithms to Stretch Audio Duration without Affecting Pitch',
+    version='0.0.1',
+    author='Joram Millenaar',
+    author_email='joormillenaar@live.nl',
+    url='https://github.com/jofoks/AudioFlex',
+    install_requires=['numpy', 'scipy'],
+    packages=[],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['audio', 'duration', 'pitch', 'music', 'stretch', 'wsola', 'overlap-add'],
+    license='MIT'
+)
